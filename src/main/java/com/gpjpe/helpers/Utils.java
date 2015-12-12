bac7bd8f3ad7923d30a3553b54ac7950aa2ead20
Guilherme Dinis Jr <guilherme.dinisjunior@gmail.com>
2015-12-12 14:22:56 +0100

    commited utils

---
+++ src/main/java/com/gpjpe/helpers/Utils.java
@@ -0,0 +1,21 @@
+package com.gpjpe.helpers;
+
+import backtype.storm.tuple.Values;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Random;
+
+public final class Utils {
+
+    public static Values tweet(Long initTimestamp) {
+
+        final String[] hashTags = new String[]{"SemperFi", "PewDiePie", "House", "Benzino"};
+        final String[] langs = new String[]{"en", "es", "it", "pt"};
+        Random random = new Random();
+
+        return new Values(langs[random.nextInt(langs.length)], hashTags[random.nextInt(hashTags.length)],
+                LocalDateTime.now().atZone(ZoneId.systemDefault()).toEpochSecond(), initTimestamp);
+    }
+
+}
